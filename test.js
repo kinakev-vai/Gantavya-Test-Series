09dc38bbe983db92a8507aed8cc5b2cc8930a834
kinakev-vai <kinakev@gmail.com>
2026-07-23 17:26:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 import { db } from "./firebase.js";
-
 import {
   collection,
   getDocs
@@ -12,26 +11,17 @@ const option3 = document.getElementById("option3");
 const option4 = document.getElementById("option4");
 
 async function loadQuestion() {
-  try {
+  const querySnapshot = await getDocs(collection(db, "questions"));
 
-    const querySnapshot = await getDocs(collection(db, "questions"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
 
-    querySnapshot.forEach((doc) => {
-
-      const data = doc.data();
-
-      question.innerText = data.question;
-      option1.innerText = data.option1;
-      option2.innerText = data.option2;
-      option3.innerText = data.option3;
-      option4.innerText = data.option4;
-
-    });
-
-  } catch (error) {
-    console.error(error);
-    alert("Question loading failed!");
-  }
+    question.innerText = data.question;
+    option1.innerText = data.option1;
+    option2.innerText = data.option2;
+    option3.innerText = data.option3;
+    option4.innerText = data.option4;
+  });
 }
 
 loadQuestion();
