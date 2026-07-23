8099163cdfec6509828f4b8d483f07853100c279
kinakev-vai <kinakev@gmail.com>
2026-07-23 15:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function startTest(){
+document.addEventListener("DOMContentLoaded", () => {
 
-alert("Test Series Coming Soon...");
+    const loginBtn = document.getElementById("loginBtn");
 
-}
+    if(loginBtn){
+        loginBtn.addEventListener("click", () => {
+            alert("Firebase Login will be added in next step.");
+        });
+    }
+
+    const registerBtn = document.getElementById("registerBtn");
+
+    if(registerBtn){
+        registerBtn.addEventListener("click", () => {
+            alert("Firebase Registration will be added in next step.");
+        });
+    }
+
+});
