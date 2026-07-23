630ff3f6eb4049dd6adb149d5e208268e00ac870
kinakev-vai <kinakev@gmail.com>
2026-07-23 15:26:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-// Firebase configuration will be added here
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3MxWODlavvudFu0utDUUCVnH1VM-UA_8",
+  authDomain: "gantavya-test-series.firebaseapp.com",
+  projectId: "gantavya-test-series",
+  storageBucket: "gantavya-test-series.firebasestorage.app",
+  messagingSenderId: "913874899973",
+  appId: "1:913874899973:web:cc73a6e0c35c05e657786c"
+};
