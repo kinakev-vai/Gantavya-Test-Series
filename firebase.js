19d82667ad832fee996bbf736936c550186226c8
kinakev-vai <kinakev@gmail.com>
2026-07-23 16:40:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,11 @@
-// Your web app's Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB3MxWODlavvudFu0utDUUCVnH1VM-UA_8",
   authDomain: "gantavya-test-series.firebaseapp.com",
@@ -6,4 +13,13 @@ const firebaseConfig = {
   storageBucket: "gantavya-test-series.firebasestorage.app",
   messagingSenderId: "913874899973",
   appId: "1:913874899973:web:cc73a6e0c35c05e657786c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 };
